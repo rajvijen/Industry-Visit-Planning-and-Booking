b88b32242e0278f20b52dd1a52edbbae158ff862
rajvijen <vijendra.cute@gmail.com>
2018-10-15 15:53:26 +0530

    updated

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'first_django_project.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ase_101.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
